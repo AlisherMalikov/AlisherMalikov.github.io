2e8ea10a81fb69e6e81cd8da45df9db3eb8cb3c5
AlisherMalikov <41441720+AlisherMalikov@users.noreply.github.com>
2019-10-14 16:41:01 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+    new WOW().init();
+    $(window).on('scroll',function(){
+        if($(window).scrollTop()){
+
+            $('.header_bottom').addClass('fixed');
+            $('.logo').addClass('fix_logo');
+        }
+        else{
+            $('.header_bottom').removeClass('fixed ');
+            $('.logo').removeClass('fix_logo');
+
+        }
+    });
+
+
+
+    $('.hamburger').on('click', function(){
+        $(this).toggleClass('is-active');
+        $('.menu').toggleClass('menu-mobile');
+    });
+
+    $('.product-card').hover(
+        
+        function() {
+          $( this ).addClass( "scale-card" );
+        }, function() {
+          $( this ).removeClass( "scale-card" );
+        }
+      );
+    
+ 
+
+      $('.product-cards').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+
+
+});
